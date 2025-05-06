6d31cd323fa9a6e525bed003556c4b1952719379
YudhoPatrianto <kydh01123@gmail.com>
2025-05-06 18:55:41 +0000

    utils: add `create_inline`
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- YudhoPRJKT/__init__.py
+++ YudhoPRJKT/__init__.py
@@ -1,6 +1,7 @@
 from .utils import (
   CreateLog,
-  GetDate
+  GetDate,
+  InlineKeyboard
 )
 from .config import (
   endpoint,
--- YudhoPRJKT/utils/__init__.py
+++ YudhoPRJKT/utils/__init__.py
@@ -1,2 +1,3 @@
 from .create_log import CreateLog
 from .get_date import GetDate
+from .create_inline import InlineKeyboard
