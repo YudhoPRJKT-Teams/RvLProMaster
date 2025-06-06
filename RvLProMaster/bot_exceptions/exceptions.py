fb49bba6319eecd93a2706c8cea4ac44be8e8dea
YudhoPatrianto <kydh01123@gmail.com>
2025-06-06 18:39:27 +0000

    exceptions: change error message
    Signed-off-by: YudhoPatrianto <kydh01123@gmail.com>

--- RvLProMaster/bot_exceptions/exceptions.py
+++ RvLProMaster/bot_exceptions/exceptions.py
@@ -8,4 +8,4 @@ class exceptions:
       super().__init__(message)
     
     def __str__(self) -> str:
-      return f"RvLProMasted Says\n{self.status_code} {self.message}"
+      return f"{self.status_code} {self.message}"
