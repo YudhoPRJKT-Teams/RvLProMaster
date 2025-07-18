d1c2797fbde0fad877ce27bb5fcc2595ce68254a
YudhoPatrianto <kydh01123@gmail.com>
2025-07-18 22:31:49 +0000

    RvLProMaster: create_log: Fix `bot.log` not found

--- RvLProMaster/utils/create_log.py
+++ RvLProMaster/utils/create_log.py
@@ -2,36 +2,42 @@ from typing import Literal
 from datetime import datetime
 import os
 import inspect
-import glob
-
-def findLog():
-    """Find the log file in the current directory and its subdirectories."""
-    for log_position in glob.iglob(f"./**/bot.log", recursive=True):
-        return os.path.abspath(log_position)
-    return None
 
 def CreateLog(
     level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"],
     message: str
 ):
-    """The Utility function for creating logs.
+    try:
+        """The Utility function for creating logs.
 
-    Args:
-        level : Log Level
-            - DEBUG: Debugging information
-            - INFO: General information
-            - WARNING: Warning messages
-            - ERROR: Error messages
-            - CRITICAL: Critical error messages
-        message (str): Your Message Wants to display into log
-    """
-    now = datetime.now()
-    day_name = now.strftime('%A')  # nama hari
-    formatted_time = now.strftime('%d-%B-%Y %I:%M:%S %p')
-    call_frame = inspect.stack()[1]
-    get_running_file = os.path.basename(call_frame.filename).replace('.py', '')
-    log_pattern = f"{day_name} {formatted_time} {level} {get_running_file} - {message}"
-    print(log_pattern)
-    with open(str(findLog()), "a") as f:
-        f.write(f"\n{log_pattern}") 
-    
+        Args:
+            level : Log Level
+                - DEBUG: Debugging information
+                - INFO: General information
+                - WARNING: Warning messages
+                - ERROR: Error messages
+                - CRITICAL: Critical error messages
+            message (str): Your Message Wants to display into log
+        """
+        now = datetime.now()
+        day_name = now.strftime('%A')  # nama hari
+        formatted_time = now.strftime('%d-%B-%Y %I:%M:%S %p')
+        call_frame = inspect.stack()[1]
+        get_running_file = os.path.basename(call_frame.filename).replace('.py', '')
+        log_pattern = f"{day_name} {formatted_time} {level} {get_running_file} - {message}"
+        print(log_pattern)
+        
+        if os.path.exists("bot.log"):
+            with open("bot.log", "a") as f:
+                f.write(f"\n{log_pattern}")
+    except FileNotFoundError:
+        with open("bot.log", "a") as f:
+            f.write(f"\n{log_pattern}")
+        os.chmod("bot.log", 0o777) # Ensure the log file is fully accessible
+    # if not os.path.exists("bot.log"):
+    #     os.chmod("bot.log", 0o777)  # Ensure the log file is fully accessible
+    #     with open("bot.log", "a") as f:
+    #         f.write(f"\n{log_pattern}")
+    # else:
+    #     with open("bot.log", "a") as f:
+    #         f.write(f"\n{log_pattern}")
