fab638c376eab33d7628fc63a4522b8cb67b2547
YudhoPatrianto <kydh01123@gmail.com>
2025-07-18 23:18:36 +0000

    RvLProMaster: save_polling: Fix missing `event.json`

--- RvLProMaster/polling/save_polling.py
+++ RvLProMaster/polling/save_polling.py
@@ -2,18 +2,23 @@ from ..utils import CreateLog
 import os
 import json
 import sys
+import subprocess
 
 
 def SavePolling(out_updates: str):
-  """save polling data to a file"""
-  base_dir = os.getcwd()
-  full_path = f"{base_dir}/event.json"
-  pretty_print = str(json.dumps(out_updates, indent=2))
-  
-  if os.path.exists(full_path):
-    with open(full_path, "w") as f:
-      f.write(pretty_print)
-  else:
-    CreateLog("ERROR", f"File Not Found")
-    with open(full_path, "w") as f:
-      f.write(pretty_print)
+  try:
+    """save polling data to a file"""
+    current_dir = os.getcwd()
+    full_path = f"event.json"
+    pretty_print = str(json.dumps(out_updates, indent=2))
+    print(f"Saving polling data to {current_dir}")
+    print(full_path)
+    if not os.path.exists(full_path):
+      with open(full_path, "w") as f:
+        f.write(pretty_print)
+    else:
+      with open(full_path, "w") as f:
+        f.write(pretty_print)
+  except Exception as e:
+    CreateLog("ERROR", f"An error occurred while saving polling data: {e}")
+    sys.exit(1)
